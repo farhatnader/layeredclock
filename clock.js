61c03cf7afdb9d1283abd57b3eecb825ce237e6c
Farhat Nader <farhat.nader01@myhunter.cuny.edu>
2016-07-10 20:24:00 -0400

    refactored clock steup and update

--- clock.js
+++ clock.js
@@ -1,25 +1,34 @@
-function generateClock() {
+function getProgress(obj) {
+	var progress = ((2*Math.PI) / obj.capacity) * obj.value;
+	return progress;
+}
+
+function setupClock() {
+	var arc_init = d3.svg.arc()
+		.startAngle(0);
+
 	var current_time = new Date();
 
 	var hr = current_time.getHours(),
 		min = current_time.getMinutes(),
 		sec = current_time.getSeconds();
 
-	var time = [
+	var time_objects = [
 		{"unit": "hrs", "value": hr, "capacity": 24, "radius": 130}, 
 		{"unit": "mins", "value": min, "capacity": 60, "radius": 165}, 
 		{"unit": "secs", "value": sec, "capacity": 60, "radius": 200}
 	];
 
-	var color = d3.scale.ordinal()
-		.domain(time)
-		.range(['#476b6b', '#006666', '#0f3e3e']);
+	return [arc_init, time_objects];
+}
 
+function generateClock(arc, data) {
 	var w = 410,
 		h = 610;
 
-	var arc = d3.svg.arc()
-		.startAngle(0);
+	var color = d3.scale.ordinal()
+		.domain(data)
+		.range(['#476b6b', '#006666', '#0f3e3e']);
 
 	var svg = d3.select("#wrapper").html('')
 		.append("svg")
@@ -28,7 +37,7 @@ function generateClock() {
 		.append("g");
 
 	var clock = svg.selectAll("hand")
-		.data(time)
+		.data(data)
 		.enter()
 		.append("g")
 			.attr('transform', 'translate(' + w / 2 + ',' + h / 2 + ')');
@@ -42,23 +51,22 @@ function generateClock() {
 		);
 	
 	clock.append("path")
-		.attr('class', "foreground")
+		.attr('class', function(d) { return d.unit; })
 		.style('fill', function(d, i) { return color(d.unit); })
 		.attr('d', arc.innerRadius(function(d) { return d.radius - 30; })
 					  .outerRadius(function(d) { return d.radius; })
 					  .endAngle(function(d) { return getProgress(d); })
 		);
 
-
 	var digital = svg.selectAll("digit")
-		.data(time)
+		.data(data)
 		.enter()
 		.append("g")
 			.attr('height', 66)
 			.attr('transform', 'translate(' + w / 2 + ',' + (h / 2 - 33) + ')');
 
 	digital.append("text")
-		.attr('class', "digit")
+		.attr('class', function(d) { return d.unit; })
 		.text(function(d) { return d.value + ' ' + d.unit; })
 		.style('fill', function(d) { return color(d.unit); })
 		.attr('transform', function(d, i) { return 'translate(0,' + i * 33 + ')'; })
@@ -66,7 +74,22 @@ function generateClock() {
 }
 
 
-function getProgress(obj) {
-	var progress = ((2*Math.PI) / obj.capacity) * obj.value;
-	return progress;
+function updateTime() {
+	var current_time = new Date();
+
+	var hr = current_time.getHours(),
+		min = current_time.getMinutes(),
+		sec = current_time.getSeconds();
+	
+	time[0].value = hr;
+	time[1].value = min;
+	time[2].value = sec;
+
+	time.forEach(function(d) {
+		d3.select("." + d.unit)
+			.attr('d', arc.endAngle(getProgress(d)));
+
+		d3.select("text." + d.unit)
+			.text(function(d) { return d.value + ' ' + d.unit; });
+	});
 }
