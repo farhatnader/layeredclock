fcea26368e5c3411f261f5687b24436320de2152
Farhat Nader <farhat.nader01@myhunter.cuny.edu>
2016-07-11 16:55:53 -0400

    implemented timezone selection

---
+++ time_config.js
@@ -0,0 +1,32 @@
+function getGroups() {
+	timezones.forEach(function(d) {
+		$("<option/>", {
+			'value': d.group,
+			'id': 'group',
+			'text': d.group,
+		}).appendTo("#groups");
+	});
+}
+
+function getZones() {
+	// var region = $("#groups").value;
+	var region = document.getElementById("groups").value;
+
+	$("#zones").html('');
+	// $("<option/>", { 
+	// 	'value': 'default',
+	// 	'text': 'Select a Location'
+	// }).appendTo("#zones");
+
+	timezones.forEach(function(d) {
+		if (d.group == region) {
+			d.zones.forEach(function(zone) {
+				$("<option/>", {
+					'value': zone.value,
+					'id': 'zone',
+					'text': zone.name
+				}).appendTo("#zones");
+			})
+		}
+	})
+}
